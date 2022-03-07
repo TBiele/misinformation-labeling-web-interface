8ef690cd83a630f8b1ee762d614d4bba36c15562
Tobias Biele <tobias.biele@gmx.de>
2022-03-07 12:11:57 +0100

    Enable admin panel message label export

--- labeling_app/admin.py
+++ labeling_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-from .models import TelegramMessage, Topic, Misconception
+from .models import TelegramMessage, Topic, Misconception, MessageMisconception
 
 class TelegramMessageResource(resources.ModelResource):
    class Meta:
@@ -23,3 +23,10 @@ class MisconceptionResource(resources.ModelResource):
 class MisconceptionAdmin(ImportExportModelAdmin):
    resource_class = MisconceptionResource
 admin.site.register(Misconception, MisconceptionAdmin)
+
+class MessageMisconceptionResource(resources.ModelResource):
+   class Meta:
+      model = MessageMisconception
+class MessageMisconceptionAdmin(ImportExportModelAdmin):
+   resource_class = MessageMisconceptionResource
+admin.site.register(MessageMisconception, MessageMisconceptionAdmin)
